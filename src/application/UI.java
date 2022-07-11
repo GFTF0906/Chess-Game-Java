ef90aaefb72940ffde05311b7d7eff6898a10f5e
Guilherme Fontebasso <guitavaresgames@gmail.com>
2022-07-11 12:47:03 -0300

    Check Logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,6 +57,10 @@ public class UI {
 		
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Wainting player: " + chessMatch.getCurrentPlayer());
+		
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
